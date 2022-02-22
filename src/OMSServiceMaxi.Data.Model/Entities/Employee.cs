8ed3b224a83a98a4f16c39b58adb0a1c519f0b88
BorisMinin <boris.minin@outlook.com>
2022-02-22 23:40:42 +0300

    Renamed Models to Entities

--- src/OMSServiceMaxi.Data.Model/Entities/Employee.cs
+++ src/OMSServiceMaxi.Data.Model/Entities/Employee.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace OMSServiceMaxi.Data.Model.Models
+﻿namespace OMSServiceMaxi.Data.Model.Entities
 {
     public class Employee
     {
