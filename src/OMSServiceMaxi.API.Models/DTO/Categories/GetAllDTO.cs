c386f238c1897990842a26964ca22bf62933df67
BorisMinin <boris.minin@outlook.com>
2022-02-23 16:42:03 +0300

    Added DTO folder with GetAllDTO

---
+++ src/OMSServiceMaxi.API.Models/DTO/Categories/GetAllDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace OMSServiceMaxi.API.Models.DTO.Categories
+{
+    public class GetAllDTO
+    {
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+    }
+}
